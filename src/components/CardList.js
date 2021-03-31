4af455f35b5d120e9918ea932ee3e8474f642b37
michaelchen <michaelchen753@gmail.com>
2021-03-31 14:32:03 +1100

    First commit

---
+++ src/components/CardList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Card from './Card';
+
+
+
+const CardList = ({robots}) => {
+    return (
+        <div>
+       { robots.map((robot, i)=> {
+        return <Card id ={robots[i].id} name ={robots[i].name} email = {robots[i].email} key ={robots[i].id}/>        
+    })}
+        </div>
+    );
+}
+
+export default CardList;
+
+
+    
+
